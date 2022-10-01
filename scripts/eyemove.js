2973661aa4b93ecf36fa6a013f04f818122fa41d
Zixi Tang <zixi.sam.tang@gmail.com>
2022-10-01 17:13:09 -0400

    testing

--- scripts/eyemove.js
+++ scripts/eyemove.js
@@ -1,8 +1,19 @@
 const eyes = document.querySelectorAll('.eye');
-const anchor = document.getElementById('cat');
-const rect = anchor.getBoundingClientRect();
+var anchor = document.getElementById('cat');
+const eye1 = document.getElementById('eye1');
+const eye2 = document.getElementById('eye2');
+const eye3 = document.getElementById('eye3');
+var rect = anchor.getBoundingClientRect();
 const anchorX = rect.left + rect.width / 2;
 const anchorY = rect.bottom + rect.height / 2;
+
+var catX = rect.left + rect.width / 5;
+var catY = rect.bottom + rect.height / 3;
+eye1.style.top = (catY - 5) + 'px';
+eye1.style.left = (catX - 140) + 'px';
+eye3.style.top = (catY) + 'px';
+eye3.style.left = (catX) + 'px';
+eye3.style.border = 'solid';
 
 document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
@@ -11,8 +22,23 @@ document.addEventListener('mousemove', (e) => {
     const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
 
     eyes.forEach(eye => {
-        eye.style.transform = 'rotate(' + (angleDeg - 90) + 'deg)';
+        eye.style.transform = 'rotate(' + (angleDeg - 100) + 'deg)';
     })
+})
+
+window.addEventListener('resize', (e) => {
+    rect = anchor.getBoundingClientRect();
+    catX = rect.left + rect.width / 5;
+    catY = rect.top + rect.height / 3;
+    
+    eye3.style.top = (catY) + 'px';
+    eye3.style.left = (catX) + 'px';
+    eye1.style.top = (catY - 80) + 'px';
+    eye1.style.left = (catX - 10) + 'px';
+    eye2.style.top = (catY - 748) + 'px';
+    eye2.style.left = (catX - 8) + 'px';
+    console.log(catY);
+    console.log(rect);
 })
 
 function angle(cursorX, cursorY, eyeX, eyeY){
