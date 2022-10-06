c2bde5700a9f0b0333486da04ee651b6ed5d9d0c
Zixi Tang <zixi.sam.tang@gmail.com>
2022-10-05 23:47:10 -0400

    made cat adaptive and fixed the front page

--- scripts/eyemove.js
+++ scripts/eyemove.js
@@ -2,43 +2,39 @@ const eyes = document.querySelectorAll('.eye');
 var anchor = document.getElementById('cat');
 const eye1 = document.getElementById('eye1');
 const eye2 = document.getElementById('eye2');
-const eye3 = document.getElementById('eye3');
-var rect = anchor.getBoundingClientRect();
-const anchorX = rect.left + rect.width / 2;
-const anchorY = rect.bottom + rect.height / 2;
+var anchorX = anchor.x + anchor.clientWidth / 10;
+var anchorY = anchor.y + anchor.clientHeight / 2.2;
 
-var catX = rect.left + rect.width / 5;
-var catY = rect.bottom + rect.height / 3;
-eye1.style.top = (catY - 5) + 'px';
-eye1.style.left = (catX - 140) + 'px';
-eye3.style.top = (catY) + 'px';
-eye3.style.left = (catX) + 'px';
-eye3.style.border = 'solid';
+eye1.style.top = (anchorY - 20) + 'px';
+eye1.style.left = (anchorX - 40) + 'px';
+eye2.style.top = (anchorY - 4) + 'px';
+eye2.style.left = (anchorX + 82) + 'px';
 
 document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
 
-    const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)
+    /*const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)*/
 
     eyes.forEach(eye => {
+        const eyeX = eye.x + eye.clientWidth / 2;
+        const eyeY = eye.y + eye.clientHeight / 2;
+        const angleDeg = angle(mouseX, mouseY, eyeX, eyeY)
         eye.style.transform = 'rotate(' + (angleDeg - 100) + 'deg)';
     })
 })
 
 window.addEventListener('resize', (e) => {
-    rect = anchor.getBoundingClientRect();
-    catX = rect.left + rect.width / 5;
-    catY = rect.top + rect.height / 3;
+    setTimeout(() => {
+        anchorX = anchor.x + anchor.clientWidth / 10;
+        anchorY = anchor.y + anchor.clientHeight / 2.2;
+
+        eye1.style.top = (anchorY - 20) + 'px';
+        eye1.style.left = (anchorX - 40) + 'px';
+        eye2.style.top = (anchorY - 5) + 'px';
+        eye2.style.left = (anchorX + 85) + 'px';
+    }, 400)
     
-    eye3.style.top = (catY) + 'px';
-    eye3.style.left = (catX) + 'px';
-    eye1.style.top = (catY - 80) + 'px';
-    eye1.style.left = (catX - 10) + 'px';
-    eye2.style.top = (catY - 748) + 'px';
-    eye2.style.left = (catX - 8) + 'px';
-    console.log(catY);
-    console.log(rect);
 })
 
 function angle(cursorX, cursorY, eyeX, eyeY){
