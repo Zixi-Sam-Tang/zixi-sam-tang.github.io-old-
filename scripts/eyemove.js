809f395c119d3839a18cbd36fecaef99dfae3f4e
Zixi Tang <zixi.sam.tang@gmail.com>
2023-01-07 15:55:48 -0500

    Finished about and contact pages

--- scripts/eyemove.js
+++ scripts/eyemove.js
@@ -1,41 +1,23 @@
 const eyes = document.querySelectorAll('.eye');
-var anchor = document.getElementById('cat');
 const eye1 = document.getElementById('eye1');
 const eye2 = document.getElementById('eye2');
-var anchorX = anchor.x + anchor.clientWidth / 10;
-var anchorY = anchor.y + anchor.clientHeight / 2.2;
 
 //tracks the mouse
 document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientY;
 
-    /*const angleDeg = angle(mouseX, mouseY, anchorX, anchorY)*/
-
     eyes.forEach(eye => {
         const eyeX = eye.x + eye.clientWidth / 2;
         const eyeY = eye.y + eye.clientHeight / 2;
+        // console.log({eye, eyeX, eyeY});
         const angleDeg = angle(mouseX, mouseY, eyeX, eyeY)
         eye.style.transform = 'rotate(' + (angleDeg - 100) + 'deg)';
     })
 })
 
-//moves the eyes to the eyesockets after window resize event
-// window.addEventListener('resize', (e) => {
-//     setTimeout(() => {
-//         anchorX = anchor.x + anchor.clientWidth / 10;
-//         anchorY = anchor.y + anchor.clientHeight / 2.2;
-
-//         eye1.style.top = (anchorY - 34) / 16 + 'rem';
-//         eye1.style.left = (anchorX - 40) / 16 + 'rem';
-//         eye2.style.top = (anchorY - 16) / 16 + 'rem';
-//         eye2.style.left = (anchorX + 82) / 16 + 'rem';
-//     }, 500)
-    
-// })
-
-//finds the angle between the center of the eye and the cursor
-function angle(cursorX, cursorY, eyeX, eyeY){
+// finds the angle between the center of the eye and the cursor
+function angle(cursorX, cursorY, eyeX, eyeY) {
     const dX = eyeX - cursorX;
     const dY = eyeY - cursorY;
 
--- scripts/gradient.js
+++ scripts/gradient.js
@@ -5,22 +5,22 @@ var colors = new Array(
   [255, 0, 255], //hot pink
   [255, 128, 0], //orange
   [210, 0, 0]); //red
-  
+
 var step = 0;
 //color table indices for: 
 // current color left (0)
 // next color left (1)
 // current color right (2)
 // next color right (3)
-var colorIndices = [0,1,2,3];
+var colorIndices = [0, 1, 2, 3];
 
 const grad = document.getElementById("gradient");
 
 //transition speed
 var gradientSpeed = 0.001;
 
-function updateGradient(){
-  
+function updateGradient() {
+
   var c0_0 = colors[colorIndices[0]];
   var c0_1 = colors[colorIndices[1]];
   var c1_0 = colors[colorIndices[2]];
@@ -31,25 +31,25 @@ function updateGradient(){
   var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
   var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
   var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
-  
+
   var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
   var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
   var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
   var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
 
   grad.style.background = 'linear-gradient(180deg, ' + color1 + ' 0%,' + color2 + ' 100%)';
-  
+
   step += gradientSpeed;
-  if (step >= 1){
+  if (step >= 1) {
     step %= 1;
     colorIndices[0] = colorIndices[1];
     colorIndices[2] = colorIndices[3];
-    
+
     //pick two new target color indices
-    colorIndices[1] = (colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    colorIndices[3] = (colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
-    
+    colorIndices[1] = (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+    colorIndices[3] = (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) % colors.length;
+
   }
 }
 
-setInterval(updateGradient,10);
+setInterval(updateGradient, 10);
